3c4eb63f5edb57119e5464e681b7fc4ee65e0ba7
alexandre campagnaro <alexandre.campagnaro>
2020-03-04 10:57:00 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import axios from 'axios'
+
+import {
+  BootstrapVue,
+  IconsPlugin
+} from 'bootstrap-vue'
+
+Vue.prototype.$http = axios.create({
+  baseURL: process.env.VUE_APP_URL
+})
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   render: h => h(App),

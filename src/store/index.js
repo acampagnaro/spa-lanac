e74e6cfe56a50a4765a96678da31663e5530a4e3
alexandre campagnaro <alexandre.campagnaro>
2020-03-16 11:05:57 -0300

    doctoralia

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as actions from './actions'
-import * as getters from './getters'
 // import * as mutations from 'mutations'
-
-import auth from './modules/auth'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     strict: process.env.NODE_ENV !== 'production',
-    actions,
-    getters,
     modules: {
-        auth
     }
 })
 
